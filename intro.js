7f772cf3b6626f3716c83286c281862c46196696
Devarsh <87245954+Shvetangdave@users.noreply.github.com>
2022-03-20 16:45:35 -0400

    ujmn

---
+++ intro.js
@@ -0,0 +1,36 @@
+
+
+function numbers(){
+    window.location="lnumber.html";
+
+}
+
+function alpha(){
+    window.location="abc.html";
+
+}
+
+function colors(){
+    window.location="fillingscreen.html";
+}
+
+function napp(){
+    window.location="about.html";
+}
+    
+function numberstest(){
+    window.location="123.html";
+
+}
+
+function colors(){
+window.location="fillingscreen.html";
+}
+
+function basic(){
+    window.location="basicwords.html";
+    }
+
+    function about(){
+        window.location="about.html";
+        }
